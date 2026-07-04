4b0470654ca8698b4e34fd69139cb078af041d37
Ojan Dewani <fauzandewani02@gmail.com>
2026-07-04 10:41:50 +0700

    update website content

--- script.js
+++ script.js
@@ -137,19 +137,3 @@ galleryItems.forEach(item=>{
   observer.observe(item);
 });
 
-const menuToggle = document.getElementById("menuToggle");
-const navbar = document.getElementById("navbar");
-
-menuToggle.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
-
-document.querySelectorAll("#navbar a").forEach(link => {
-
-    link.addEventListener("click", () => {
-
-        navbar.classList.remove("active");
-
-    });
-
-});
